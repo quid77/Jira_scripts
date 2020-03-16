001dcf93664c92c0f04f78c05045c5c2c441c94a
quid77 <l0quid16@gmail.com>
2020-03-16 18:28:51 +0100

    TODO: fix downloading edge cases, add dir hierarchy

--- Jira_downloads.py
+++ Jira_downloads.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import getpass
@@ -8,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 import shutil
 import difflib
+from docx.shared import Inches, Pt
 import glob
 import re
 import win32com.client as win32
@@ -18,8 +20,8 @@ import unittest
 
 
 download_path = "C:\\PycharmProjects\\Jira_scripts\\Downloads";
-user_login = "abc"
-user_password = "abc"
+user_login = ""
+user_password = ""
 
 
 class JiraTestsDownload(unittest.TestCase):
@@ -56,28 +58,39 @@ class JiraTestsDownload(unittest.TestCase):
         search.send_keys("Updated")
         wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='updated-1']/label"))) # wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@title='Updated']")))
         search.send_keys(Keys.ENTER)
-
         time.sleep(3)
-
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "order-options")))
-        wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "issue-link-key")))
-        wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@class='issue-link-key']")))
 
     def test_3_download_tests(self):
         driver = self.driver
-        wait = WebDriverWait(driver, 10)
-        print("it starts now")
-        wait.until(EC.element_to_be_clickable((By.XPATH, "(//*[@class='issue-link-key'])[1]")))
-        tests_list = driver.find_elements_by_class_name("issue-link-key")
-        print("Number of files: ", len(tests_list))   # looks obsolete, won't work for > 50 files as it checks only 1 page
-        for test_element in tests_list:
-            test_element.click()
-            time.sleep(2)
-            wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='viewissue-export']")))
-            driver.find_element_by_xpath("//*[@id='viewissue-export']").click()
-            wait.until(EC.element_to_be_clickable((By.XPATH,"//*[@id='jira.issueviews:issue-word']")))
-            driver.find_element_by_xpath("//*[@id='jira.issueviews:issue-word']").click()
-        time.sleep(3)
+        wait = WebDriverWait(driver, 2)
+        while True:
+            tests_list = driver.find_elements_by_class_name("splitview-issue-link")
+            for test_element in tests_list:
+                test_element.click()
+                for x in range(0, 10):
+                    try:  # DOuble space, ", break the script
+                        test_element_title = test_element.find_element_by_xpath(".//span[@class='issue-link-summary']").text
+                        wait.until(EC.presence_of_element_located((By.XPATH, "//h1[text()[contains(.,\"%s\")]]" % test_element_title)))
+                        break
+                    except (StaleElementReferenceException, TimeoutException):
+                        time.sleep(0.2)
+                        continue
+                for x in range(0, 10):
+                    try:
+                        wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='viewissue-export']")))
+                        driver.find_element_by_xpath("//*[@id='viewissue-export']").click()
+                        wait.until(EC.element_to_be_clickable((By.XPATH, "//*[@id='jira.issueviews:issue-word']")))
+                        driver.find_element_by_xpath("//*[@id='jira.issueviews:issue-word']").click()
+                        break
+                    except (StaleElementReferenceException, TimeoutException):
+                        time.sleep(0.2)
+                        continue
+            try:
+                wait.until(EC.element_to_be_clickable((By.XPATH, "//a[@class='icon icon-next']")))
+                driver.find_element_by_xpath("//a[@class='icon icon-next']").click()
+                time.sleep(3)
+            except NoSuchElementException:
+                break
         print("Downloading done")
 
 
@@ -109,7 +122,7 @@ def move_files():
 # paths_to_files = os.listdir(r"C:/PycharmProjects/Jira_scripts/Downloads/DocFiles")
 
 # this function isn't standalone, use save_to_docx instead
-def save_as_docx(paths_to_files):  # Github convertion solution
+def save_as_docx(paths_to_files):  # Github conversion solution
     # Opening MS Word
     word = win32.gencache.EnsureDispatch('Word.Application')
     doc = word.Documents.Open(paths_to_files)
@@ -124,9 +137,9 @@ def save_as_docx(paths_to_files):  # Github convertion solution
     doc.Close(False)
 
 
-def save_to_docx():
-    paths_to_file = os.listdir(r"C:\PycharmProjects\Jira_scripts\Downloads\DocFiles")
-    paths_to_files = [download_path + "\\DocFiles\\" + path for path in paths_to_file]
+def save_to_docx():  # chceck obs if it doesnt work
+    file_names = os.listdir(r"C:\PycharmProjects\Jira_scripts\Downloads\DocFiles")
+    paths_to_files = [download_path + "\\DocFiles\\" + name for name in file_names]
     for path in paths_to_files:
         save_as_docx(path)
 
@@ -136,7 +149,13 @@ def remove_doc_files():
     for file in filenames:
         if file.endswith(".doc"):
             os.remove(file)
-    time.sleep(3)
+
+    #  Remove existing RWS filled-template files
+    os.chdir(download_path + "\\TestTemplates")
+    for filename in os.listdir(download_path + "\\TestTemplates"):
+        if filename.startswith("RWS"):
+            os.remove(filename)
+    time.sleep(2)
 
 
 def read_docx_files():
@@ -146,120 +165,71 @@ def read_docx_files():
         docx_handler = docx.Document(file)
         docx_tables = docx_handler.tables
         title = docx_tables[0].rows[0].cells[0].text
-        print(title)
-        title_split = title.split()
-        print(title_split)
-        jira_test_id = title_split[0]
-        print(jira_test_id)
+        jira_test_id = title.split()[0]
         zephyr_teststeps = docx_tables[2].rows[2].cells[0].text
-        print(zephyr_teststeps)
-        if zephyr_teststeps == "Zephyr Teststep:":
+        if "Zephyr" in zephyr_teststeps:
             zephyr_tests = docx_tables[2].rows[2].cells[1]
         else:
             zephyr_tests = docx_tables[2].rows[3].cells[1]
         zephyr_tests_table = zephyr_tests.tables
-        number_of_teststeps = zephyr_tests_table[0].rows[-1].cells[0].text  # teststep 1???
+        #
+        # number_of_teststeps = zephyr_tests_table[0].rows[-1].cells[0].text
+        # print(number_of_teststeps)
 
-        print(number_of_teststeps)
+        zephyr_rows = zephyr_tests_table[0].rows  # get row id's
+        zephyr_rows = zephyr_rows[1:]  # remove first cell from all rows (e.g. "Test Step", "Test Data", etc.)
 
-        tab_zef = zephyr_tests_table[0].rows
-        tab_zef = tab_zef[1:]  # obcinanie tytulu test stepow
+        #  Test Steps
+        list_of_test_steps = []
+        for row in zephyr_rows:
+            list_of_test_steps.append(row.cells[1].text)
 
-        lista_stepow = []
-        for row in tab_zef:
-            lista_stepow.append(row.cells[1].text)
+        #  Test Conditions
+        list_of_test_conditions = []
+        for row in zephyr_rows:
+            list_of_test_conditions.append(row.cells[2].text)
 
-        lista_stepow[0]
-        #Test Condition
-        lista_TestCondition=[]
+        #  Expected results
+        list_of_exptected_results = []
+        for row in zephyr_rows:
+            list_of_exptected_results.append(row.cells[3].text)
 
-        for row in tab_zef:
-            lista_TestCondition.append(row.cells[2].text)
+        number_of_teststeps = zephyr_tests_table[0].rows[-1].cells[0].text
+        file_save_path = download_path + "\\TestTemplates\\RWS-" + os.path.basename(file)
 
-     lista_ExpResult=[]
-#     for row in tab_zef:
-#         lista_ExpResult.append(row.cells[3].text)
-#     if step1 == "2":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts.docx")
-#         table1 = doc1.tables
-#     elif step1 == "3":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts3.docx")
-#         table1 = doc1.tables
-#     elif step1 == "4":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts4.docx")
-#         table1 = doc1.tables
-#     elif step1 == "5":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts5.docx")
-#         table1 = doc1.tables
-#     elif step1 == "6":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts6.docx")
-#         table1 = doc1.tables
-#     elif step1 == "7":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts7.docx")
-#         table1 = doc1.tables
-#     elif step1 == "8":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts8.docx")
-#         table1 = doc1.tables
-#     elif step1 == "9":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts9.docx")
-#         table1 = doc1.tables
-#     elif step1 == "10":
-#         doc1 = docx.Document("C:\\Users\\jpiet\\Downloads\\SampleTestScripts10.docx")
-#         table1 = doc1.tables
-#
-#     idTest1 = table1[0].rows[0].cells[2]
-#     add_idScenario = idTest1.add_paragraph(idTest)
-#     user_story = table1[0].rows[4].cells[2]
-#     add_UserStory = user_story.add_paragraph(idTest)
-#
-#     tabela_template = table1[0].rows
-#
-#     tabela_template = tabela_template[6:]
-#
-#     lista_TestStepow = []
-#     for row in tabela_template:
-#         lista_TestStepow.append(row.cells[1])
-#
-#
-#         #Test Stepy
-#     index = 0
-#     for element in lista_TestStepow:
-#         element.add_paragraph(lista_stepow[index])
-#         index +=1
-#
-#         #Test Condition
-#     lista_TestCondition1 =[]
-#     for row in tabela_template:
-#         lista_TestCondition1.append(row.cells[2])
-#     index = 0
-#     for element in lista_TestCondition1:
-#         element.add_paragraph(lista_TestCondition[index])
-#         index +=1
-#
-#         #Dodawanie test Condition
-#
-#     lista_ExpResult1 =[]
-#     for row in tabela_template:
-#         lista_ExpResult1.append(row.cells[3])
-#
-#     index = 0
-#     for element in lista_ExpResult1:
-#         element.add_paragraph(lista_ExpResult[index])
-#         index +=1
-#
-#         #Dodawanie Excepted Result
-#     doc1.save(file)
+        rws_template = docx.Document(download_path + "\\SampleTestScripts1.docx")
+        rws_table = rws_template.tables
+        font = rws_template.styles['Normal'].font
+        font.name = 'Calibri'
+        paragraph = rws_template.styles['Normal'].paragraph_format
+        paragraph.space_after = Pt(3)
+        for x in range(1, int(number_of_teststeps)):
+            rws_table[0].add_row()
+
+        os.chdir(download_path)
+        test_id = rws_table[0].rows[0].cells[2].paragraphs[0]
+        test_id.add_run(jira_test_id)
+        user_story = rws_table[0].rows[4].cells[2].paragraphs[0]
+        user_story.add_run(jira_test_id)
+
+        steps_only_table = rws_table[0].rows[6:]
+
+        for x in range(0, int(number_of_teststeps)):
+            steps_only_table[x].cells[1].paragraphs[0].add_run(list_of_test_steps[x])
+        for x in range(0, int(number_of_teststeps)):
+            steps_only_table[x].cells[2].paragraphs[0].add_run(list_of_test_conditions[x])
+        for x in range(0, int(number_of_teststeps)):
+            steps_only_table[x].cells[3].paragraphs[0].add_run(list_of_exptected_results[x])
+
+        rws_template.save(file_save_path)
 
 
 if __name__ == "__main__":
 
-    # unittest.main()
-    # paths = glob.glob('C:/PycharmProjects/Jira_scripts/Downloads/DocFiles/**/*.doc', recursive=True)
-    # paths = os.listdir(r"C:/PycharmProjects/Jira_scripts/Downloads/DocFiles")
-    # paths_to_files = os.listdir(r"C:/PycharmProjects/Jira_scripts/Downloads/DocFiles")
+    unittest.main()
     # save_to_docx()
     # remove_doc_files()
     # read_docx_files()
-    read_docx_files()
+    # read_docx_files()
 
 
